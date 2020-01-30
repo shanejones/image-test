c7c492f1c77f8c41d72573f3c2873aee8258e6be
Shane Jones <shane@under2.agency>
2020-01-30 15:38:29 +0000

    initial commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'production' => false,
+    'baseUrl' => '',
+    'siteTitle' => 'Jigsaw + Tailwind CSS Starter Kit',
+    'collections' => [],
+];
---
+++ source/index.blade.php
@@ -0,0 +1,10 @@
+@extends('_layouts.master')
+
+@section('content')
+
+<main class="main">
+    <h1 class="font-light text-3xl text-black">Jigsaw + Tailwind CSS</h1>
+    <p class="text-gray-600 mt-4">A starter kit for using <a href="https://tailwindcss.com/">Tailwind CSS</a> with <a href="http://jigsaw.tighten.co/">Jigsaw</a></p>
+</main>
+
+@endsection
